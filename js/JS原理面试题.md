78dbfb0d1ca0274a0fd501cc78c080e79d4416b6
yuchao226 <YUCHAO226@pingan.com.cn>
2019-11-05 14:44:23 +0800

    又一波新知识

--- js/JS原理面试题.md
+++ js/JS原理面试题.md
@@ -20,17 +20,200 @@
    </code>
    </pre>
 * 2. 实现一个apply函数
+  <pre>
+  <code>
+    Function.prototype.myapply = function(context) {
+      if(typeof this !== 'function') {
+        throw new Error('not function');
+      }
+      context = context || window;
+      context.fn = this;
+      let args = arguments[1];
+      let result;
+      if(args) {
+        result = context.fn(...args);
+      }
+      else {
+        result = context.fn();
+      }
+      delete context.fn;
+      return result;
+    }
+  </code>
+  </pre>
 * 3. 实现一个bind函数
+  
 * 4. instanceof的原理
 * 5. Object.create的基本实现原理
 * 6. new本质
 * 7. 实现一个基本的Promise
+  ```
+  class Promise {
+    constructor() {
+      this.sucList = [];
+      this.failList = [];
+      this.state = 'pending';
+    }
+    then(sucCallBack, failCallBack) {
+      this.sucList.push = [];
+    }
+    resolve() {
+
+    }
+  }
+  <!-- test demo -->
+  new Promise((resolve, reject) => {
+
+  })
+  .then(data => {
+    console.log('data => ', data);
+  })
+  ```
 * 8. 实现浅拷贝
 * 9. 实现一个基本的深拷贝
+  + 1、JSON.parse(JSON.stringify(obj))
+  弊端和局限性：
+    + 无法处理 function，还有 RegExp
+    + 无法处理循环引用对象
+    + 会丢弃对象的constructor
 * 10. 使用setTimeout模拟setInterval
 * 11. js实现一个继承方法
+  ```
+  父类：
+  function Parent() {
+    this.type = 'father';
+  }
+  Parent.prototype.work = function() {console.log('000000')}
+  ```
+  + 1、借助call；弊端，无法获得父类原型对象上的方法
+  ```
+  function Children1() {
+    Parent.call(this);
+  }
+  new Children1();
+  ```
+  + 2、借助原型链继承；弊端，所有子类实例公继承同一套父类属性，导致子类修改属性时会互相影响
+  ```
+  function Children2() {
+
+  }
+  Children2.prototype = new Parent();
+  new Children2();
+  ```
+  + 3、组合call和原型链进行继承；弊端，Parent构造方法执行了多了一次啊，导致Children3.prototype上多了一套父类属性，就是这一步 {Children3.prototype = new Parent();} 造成的
+  ```
+  function Children3() {
+    Parent.call(this);
+  }
+  Children3.prototype = new Parent();
+  Children3.constructor = Children3;
+  console.log(new Children3());
+  ```
+  + 4、继承最优解 (寄生组合继承)
+  ```
+  function Children4() {
+    Parent.call(this);
+  }
+  方案一：
+  function Fn() {
+    <!-- 空 -->
+  }
+  Fn.prototype = Parent.prototype;
+  Children4.prototype = new Fn();
+  方案二：
+  Children4.prototype = Object.create(Parent.prototype);
+
+  Children4.prototype.constructor = Children4;
+  console.log(new Children4())
+  ```
+  <b>追问: ES6中继承的实现原理是什么？</b>
+  也是使用寄生组合继承，并且使用 Object.setPrototypeOf(subClass, superClass)，复制了父类的静态方法.
+  ```
+  原理：
+  Object.setPrototypeOf: 设置一个指定对象的隐式原型（即，内部的[__proto__]）到另一个对象。
+  如果浏览时不支持该方法，兼容写法即 subClass.__proto__ = superClass;
+  ```
+  <b>追问: 面向对象的设计一定是好的设计吗？</b> 那不一定
+  > 继承的最大问题在于：无法决定继承哪些属性，所有属性都得继承。
+
+  <b>追问: 那如何来解决继承的诸多问题呢？</b>
+  用组合，采用面向组合的设计方式。
+  > 顾名思义，面向组合就是先设计一系列零件，然后将这些零件进行拼装，来形成不同的实例或者类。
+
 * 12. 实现一个基本的Event Bus（订阅、发布模式）
+  ```
+  const EventEmitter = {
+    events: {},
+    // 订阅
+    on: function(eventName, cb, once=false) {
+      if (typeof cb !== 'function') {
+        throw new Error('cb 不是一个函数！');
+      }
+      !this.events[eventName] && (this.events[eventName] = []);
+      this.events[eventName].push({fn: cb, once});
+    },
+    // 触发
+    emit: function(eventName, ...args) {
+      const cblist = this.events[eventName];
+      // 找到已订阅的事件
+      if (cblist) {
+        for (let index = 0; index < cblist.length; index++) {
+          cblist[index].fn(...args);
+          if (cblist[index].once) {
+            cblist.splice(index, 1);
+          }
+        }
+      }
+    },
+    // 清除订阅
+    off: function(eventName, removeCb) {
+      if (eventName && this.events[eventName]) {
+        // 具体删除某一次订阅
+        if (removeCb) {
+          const index = this.events[eventName].findIndex(event => event.fn === removeCb);
+          index > -1 && this.events[eventName].splice(index, 1);
+        }
+        else {
+          this.events[eventName] = [];
+        }
+      }
+      else {
+        // 清空全部订阅
+        this.events = {};
+      }
+    },
+    // 订阅一次
+    once: function(eventName, cb) {
+      this.on(eventName, cb, true);
+    }
+  }
+  ```
 * 13. 实现一个双向数据绑定
+  + 简单版
+  ```
+  var span = document.getElementById('span');
+  var input = document.getElementById('input');
+  var obj = {};
+  Object.defineProperty(obj, 'value', {
+    configurable: false,
+    enumerable: false,
+    get() {
+      console.log('获取数据');
+      return value
+    },
+    set(newVal) {
+      console.log('数据更新');
+      input.value = newVal;
+      span.innerText = newVal;
+    }
+  })
+  input.addEventListener('keyup', function(e) {
+    obj.value = e.target.value;
+  })
+  ```
+  + 复杂版
+  [https://github.com/YuFy1314/simpleVue]()
+  <b>扩展知识：vue 依赖收集是在created生命钩子执行</b>
 * 14. 实现一个简单路由
 * 15. 实现懒加载
 * 16. rem基本设置
@@ -79,3 +262,26 @@
       }
     </code>
   </pre>
+
+* 23、http请求三次握手与4次具体过程
+  <img src=https://upload-images.jianshu.io/upload_images/5291509-f1e796c2d96f2b84.png?imageMogr2/auto-orient/strip|imageView2/2/w/505/format/webp>
+
+  <b>知识扩展:</b>
+  HTTP1、HTTP1.1、HTTP2.0分别代表什么意思？
+
+  HTTP2 优势：
+  * HTTP2与HTTP1.1最重要的区别就是解决了<b>线头阻塞</b>的问题！
+  * HTTP2所有性能增强的核心在于<b>新的二进制分帧层</b>(不再以文本格式来传输了)，它定义了如何封装http消息并在客户端与服务器之间传输
+<br>
+* 24、js forEach方法中，this指向谁？如何改变this？
+  ```
+  array.forEach(function(currentVal, index, arr), thisValue);
+  一般方法中this都指向window。可以使用thisValue， 表示给函数传递使用的this，修改函数中的this
+  ```
+* 25、实现数组的flat, reduce方法
+  ```
+  ```
+  
+* 26、react hooks的实现原理
+* 27、二叉树前、中、后遍历原理？知道前序遍历和中序结果，请问后序遍历结果是什么？
+* 28、react 路由中hash值起什么作用？
--- js/test.js
+++ js/test.js
@@ -12,6 +12,239 @@ function randomArr(arr, lg) {
     ];
   }
 }
-console.log(randomArr(testArr, 5))
+// console.log(randomArr(testArr, 5))
 
 // 将树形结构数据通过递归给出树状图
+const treeData = [
+  {
+    name: 'xx1',
+    children: [
+      {
+        name: 'yy1-1'
+      },
+      {
+        name: 'yy1-2'
+      }
+    ]
+  },
+  {
+    name: 'xx2',
+    children: [
+      {
+        name: 'yy2-1'
+      },
+      {
+        name: 'yy2-2'
+      }
+    ]
+  }
+]
+
+// 深拷贝
+function deepClone(initObj) {
+  let obj;
+  if (typeof initObj === 'object') {
+    obj = initObj.constructor === Array ? [] : {};
+    for (let key in initObj) {
+      if (initObj.hasOwnProperty(key)) {
+        obj[key] = typeof initObj[key] === 'object' 
+          ? deepClone(initObj[key])
+          : initObj[key]
+      }
+    }
+  }
+  return obj;
+}
+// console.log(deepClone(treeData))
+
+// 实现一个 instance 运算
+// 核心是沿着原型链往上找
+function judgeType(data, type) {
+  // 对于基本数据类型，直接返回 false。因为 instance 判断不了原始数据类型（Number,String,Boolean）
+  if (typeof data !== 'object' || typeof data === null) return false;
+  // 获取数据的原型对象
+  let proto = Object.getPrototypeOf(data);
+  while(proto) {
+    // 找到相同的原型对象
+    if (proto === type.prototype) return true;
+    proto = Object.getPrototypeOf(proto);
+    // 查找到尽头，也没有找到
+    if (proto === null) return false;
+  }
+}
+
+// console.log(judgeType({}, Object))
+
+// 订阅发布模式
+const EventEmitter = {
+  events: {},
+  // 订阅
+  on: function(eventName, cb, once=false) {
+    if (typeof cb !== 'function') {
+      throw new Error('cb 不是一个函数！');
+    }
+    !this.events[eventName] && (this.events[eventName] = []);
+    this.events[eventName].push({fn: cb, once});
+  },
+  // 触发
+  emit: function(eventName, ...args) {
+    const cblist = this.events[eventName];
+    // 找到已订阅的事件
+    if (cblist) {
+      for (let index = 0; index < cblist.length; index++) {
+        cblist[index].fn(...args);
+        if (cblist[index].once) {
+          cblist.splice(index, 1);
+        }
+      }
+    }
+  },
+  // 清除订阅
+  off: function(eventName, removeCb) {
+    if (eventName && this.events[eventName]) {
+      // 具体删除某一次订阅
+      if (removeCb) {
+        const index = this.events[eventName].findIndex(event => event.fn === removeCb);
+        index > -1 && this.events[eventName].splice(index, 1);
+      }
+      else {
+        this.events[eventName] = [];
+      }
+    }
+    else {
+      // 清空全部订阅
+      this.events = {};
+    }
+  },
+  // 订阅一次
+  once: function(eventName, cb) {
+    this.on(eventName, cb, true);
+  }
+}
+function getlist1(...params) {
+  console.log('getlist---1', params)
+}
+function getlist2(...params) {
+  console.log('getlist---2', params)
+}
+function getlist3(...params) {
+  console.log('getlist---3', params)
+}
+// 订阅
+// EventEmitter.on('fresh', getlist1);
+// EventEmitter.on('fresh', getlist2);
+// EventEmitter.once('fresh', getlist3);
+
+// EventEmitter.emit('fresh', [1,2,3,4]);
+
+// 手写一个promise构造方法
+class Promise {
+  constructor(fn) {
+    this.sucList = [];
+    this.failList = [];
+    this.state = 'pending';
+    this.resolve.bind(this);
+    this.reject.bind(this);
+    fn(this.resolve, this.reject);
+  }
+  static all(proArr) {
+
+  }
+  then(sucCallBack, failCallBack) {
+    // 调用.then方法会立刻返回一个promise实例，继续链式调用
+    return new Promise((resolve, reject) => {
+      function suc(value) {
+        // 判断当前 .then 方法是否再次返回了promise实例。如果是则注册then方法
+        let ret = typeof sucCallBack === 'function' && sucCallBack(value) || value;
+        if (ret && typeof ret['then'] === 'function') {
+          ret.then((value) => {
+            this.resolve(value);
+          });
+        }
+        else {
+          this.resolve(value);
+        }
+      }
+      if (this.state === 'pending') {
+        this.sucList.push(suc);
+        this.failList.push(failCallBack);
+      }
+    })
+  }
+  resolve(value) {
+    this.state = 'success';
+    // 保证then方法已经执行，回调函数已经注入
+    setTimeout(() => {
+      this.sucList.forEach(cb => cb(value));
+    }, 0);
+  }
+  reject(reason) {
+    this.state = 'failed';
+    setTimeout(() => {
+      this.failList.forEach(cb => cb(reason));
+    }, 0);
+  }
+}
+
+// 测试 promise方法
+// new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(99);
+//   }, 1000);
+// })
+// .then(data => {
+//   console.log('data => ', data);
+// })
+
+/**
+ * 实现js中的继承
+ */
+
+// 1、借助call。【无法获取父类原型对象上的方法】
+function Parent1(params) {
+  this.name = 'www';
+  this.num = [1,2,3];
+}
+Parent1.prototype.ask = function (params) {
+  console.log('I ask')
+}
+function Children1(params) {
+  Parent1.call(this);
+  this.age = 19;
+}
+var child1 = new Children1();
+var parent1 = new Parent1();
+// parent1.ask();
+// console.log('child1', child1)
+
+// 2、借助原型链 【原型对象相同，修改属性会互相影响；构造函数是Parent2，不是Child2】
+function Parent2(params) {
+  this.name = 'www';
+  this.num = [1,2,3];
+}
+Parent2.prototype.ask = function (params) {
+  console.log('I ask for 2')
+}
+function Children2(params) {
+  // Parent2.call(this);
+  this.age = 22;
+}
+Children2.prototype = new Parent2();
+var child2 = new Children2();
+var child2_2 = new Children2();
+child2_2.num.push(99);
+// console.log('=====', child2.num, child2_2.num)
+
+// 3、组合继承优化（寄生组合继承）
+function Parent3(params) {
+  this.name = 'www';
+  this.num = [1,2,3];
+}
+function Children3(params) {
+  Parent3.call(this);
+  this.age = 30;
+}
+Children3.prototype = Object.create(Parent3.prototype);
+Children3.prototype.constructor = Children3;
+var child3 = new Children3();
+console.log('child3', child3)
